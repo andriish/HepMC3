5961fafd3a5bbfc4b1fa40cc94fb5c6295e9a409
Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>
2023-12-17 02:00:43 +0100

    OK

--- src/ReaderAscii.cc
+++ src/ReaderAscii.cc
@@ -218,7 +218,7 @@ bool ReaderAscii::read_event(GenEvent &evt) {
 
     }
     std::unordered_map<int, std::pair< std::set<int>, std::set<int> > >  m_io_particles_minus;
-    long currid = -(long)m_data.vertices.size();
+    int currid = -static_cast<int>(m_data.vertices.size());
     auto fir = m_io_particles_plus.begin();
     for (const auto& io: m_io_particles) {
         for (;currid<io.first;++currid) {
