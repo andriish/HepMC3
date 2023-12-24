97fda16f4fb76ae35eb65003933047b3f563dde1
Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>
2023-12-24 23:35:22 +0100

    Regenerate python bindings and fix cmake config for python

--- python/src/pyHepMC3search.cpp
+++ python/src/pyHepMC3search.cpp
@@ -9,7 +9,6 @@
 
 typedef std::function< pybind11::module & (std::string const &) > ModuleGetter;
 
-void bind_pyHepMC3search_0(std::function< pybind11::module &(std::string const &namespace_) > &M);
 
 
 PYBIND11_MODULE(pyHepMC3search, root_module) {
@@ -34,12 +33,10 @@ PYBIND11_MODULE(pyHepMC3search, root_module) {
 	);
 
 	std::vector< std::pair<std::string, std::string> > sub_modules {
-		{"", "HepMC3"},
 	};
 	for(auto &p : sub_modules ) modules[p.first.size() ? p.first+"::"+p.second : p.second] = modules[p.first].def_submodule( mangle_namespace_name(p.second).c_str(), ("Bindings for " + p.first + "::" + p.second + " namespace").c_str() );
 
 	//pybind11::class_<std::shared_ptr<void>>(M(""), "_encapsulated_data_");
 
-	bind_pyHepMC3search_0(M);
 
 }
