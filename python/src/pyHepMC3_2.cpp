ee0663b1f5deac8fd4b551377331d8790cda73eb
Andrii Verbytskyi <andrii.verbytskyi@mpp.mpg.de>
2024-05-24 20:50:56 +0200

    OK

--- python/src/pyHepMC3_2.cpp
+++ python/src/pyHepMC3_2.cpp
@@ -1,4 +1,15 @@
-#include <HepMC3/FourVector.h>
+#include <HepMC3/Attribute.h>
+#include <HepMC3/GenCrossSection.h>
+#include <HepMC3/GenHeavyIon.h>
+#include <HepMC3/GenVertex.h>
+#include <functional>
+#include <iterator>
+#include <map>
+#include <memory>
+#include <set>
+#include <sstream> // __str__
+#include <string>
+#include <utility>
 
 #include <functional>
 #include <pybind11/pybind11.h>
@@ -20,7 +31,22 @@
 
 void bind_pyHepMC3_2(std::function< pybind11::module &(std::string const &namespace_) > &M)
 {
-	// HepMC3::delta_r_rap(const class HepMC3::FourVector &, const class HepMC3::FourVector &) file:HepMC3/FourVector.h line:
-	M("HepMC3").def("delta_r_rap", (double (*)(const class HepMC3::FourVector &, const class HepMC3::FourVector &)) &HepMC3::delta_r_rap, "R_rap-distance separation dR = sqrt(dphi^2 + drap^2) between vecs  and \n\nC++: HepMC3::delta_r_rap(const class HepMC3::FourVector &, const class HepMC3::FourVector &) --> double", pybind11::arg("a"), pybind11::arg("b"));
+	// std::map file:bits/stl_map.h line:
+	binder::map_binder<std::string,int,std::less<std::string >,std::allocator<std::pair<const std::string, int> >>(M("std"), "std_string", "int", "std_less_std_string_t", "std_allocator_std_pair_const_std_string_int_t");
+
+	// std::map file:bits/stl_map.h line:
+	binder::map_binder<std::string,std::shared_ptr<HepMC3::Attribute>,std::less<std::string >,std::allocator<std::pair<const std::string, std::shared_ptr<HepMC3::Attribute> > >>(M("std"), "std_string", "std_shared_ptr_HepMC3_Attribute_t", "std_less_std_string_t", "std_allocator_std_pair_const_std_string_std_shared_ptr_HepMC3_Attribute_t");
+
+	// std::map file:bits/stl_map.h line:
+	binder::map_binder<std::string,std::map<int, std::shared_ptr<HepMC3::Attribute> >,std::less<std::string >,std::allocator<std::pair<const std::string, std::map<int, std::shared_ptr<HepMC3::Attribute> > > >>(M("std"), "std_string", "std_map_int_std_shared_ptr_HepMC3_Attribute_t", "std_less_std_string_t", "std_allocator_std_pair_const_std_string_std_map_int_std_shared_ptr_HepMC3_Attribute_t");
+
+	// std::map file:bits/stl_map.h line:
+	binder::map_binder<std::string,std::string,std::less<std::string >,std::allocator<std::pair<const std::string, std::string > >>(M("std"), "std_string", "std_string", "std_less_std_string_t", "std_allocator_std_pair_const_std_string_std_string_t");
+
+	// std::map file:bits/stl_map.h line:
+	binder::map_binder<std::shared_ptr<const HepMC3::GenVertex>,int,std::less<std::shared_ptr<const HepMC3::GenVertex> >,std::allocator<std::pair<const std::shared_ptr<const HepMC3::GenVertex>, int> >>(M("std"), "std_shared_ptr_const_HepMC3_GenVertex_t", "int", "std_less_std_shared_ptr_const_HepMC3_GenVertex_t", "std_allocator_std_pair_const_std_shared_ptr_const_HepMC3_GenVertex_int_t");
+
+	// std::map file:bits/stl_map.h line:
+	binder::map_binder<std::string,std::set<long>,std::less<std::string >,std::allocator<std::pair<const std::string, std::set<long> > >>(M("std"), "std_string", "std_set_long_t", "std_less_std_string_t", "std_allocator_std_pair_const_std_string_std_set_long_t");
 
 }
